810c53fed578932e84ead097991724196bc24e05
Daiming Yang <yang.daim@northeastern.edu>
2022-11-16 21:19:28 -0500

    Adds inventory

---
+++ work/react-intro/inventory/src/Reorder.jsx
@@ -0,0 +1,9 @@
+
+function Reorder({ onReorder }) {
+    return (
+        <button type='button' className='buttons--reorder' onClick={onReorder}
+        >Reorder</button>
+    );
+
+};
+export default Reorder;
